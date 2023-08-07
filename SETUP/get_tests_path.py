5ee4d9e29602b7f49507735854c8d5a6cfcd08b4
John Strickler <jstrickler@gmail.com>
2023-08-07 09:14:31 -0400

    updated script

--- SETUP/get_tests_path.py
+++ SETUP/get_tests_path.py
@@ -5,20 +5,16 @@ import os
 
 root_folder = os.path.dirname(os.getcwd())
 
+print(f"root_folder: {root_folder}")
+
+
 for folder in 'examples', 'EXAMPLES':
     examples_folder = os.path.join(root_folder, folder)
     if os.path.exists(examples_folder):
+        test_folder = os.path.join(root_folder, 'EXAMPLES', 'tests')
+        print(test_folder)
         break
 else:
-    print("examples folder not found")
+    print("EXAMPLES folder not found")
     exit(1)
-
-for folder, subfolders, filenames in os.walk(examples_folder):
-    if folder.endswith('tests'):
-        print(folder)
-        break
-else:
-    print("test folder not found!")
-    exit(1)
-
 
